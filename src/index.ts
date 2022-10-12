ec7cea3cfd52bee9ab7efb9970c9b4eb2cacab07
Quentin Caritey <quentin.caritey@neoxia.com>
2022-10-12 23:30:01 +0200

    form bmp to png

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import * as fs from 'fs';
-import { Bitmap } from 'bitmap-ts';
+import { PNG } from 'pngjs';
 
-const file = fs.readFileSync('assets/provinces.bmp');
-const bmp = new Bitmap(new File([], 'provinces.bmp'));
+type Color = string;
 
-console.log(bmpBuffer);
+const iter = function*(n: number) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+};
+
+const map = new Map<Color, string[]>();
+const img = fs.readFileSync('assets/provinces.png');
+const png = PNG.sync.read(img);
+for (const y of iter(png.height)) {
+  for (const x of iter(png.width)) {
+    const index = (png.width * y + x) << 2;
+    const color = `${png.data[index]};${png.data[index + 1]};${png.data[index + 2]}`;
+    map.set(
+      color,
+      [ ...(map.get(color) ?? []), `${x};${y}` ]
+    );
+  }
+}
+
+console.log(map);
+
+// new PNG().parse(img, (_, data) => {
+//   for ()
+//   console.log(data.data);
+// });
